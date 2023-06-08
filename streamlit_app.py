8e88c8d78ad49b49202182d013fdd53cde6f3200
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-09 01:31:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,15 @@ import requests
 
 with st.form("my_form"):
     st.header("Med Affairs Outreach Email Generator")
-    st.text_input("Input text", key="name")
+    option = st.selectbox(
+        'Patient Name',
+        ('James', 'Warren'))
     submitted = st.form_submit_button("Submit")
 
 # You can access the value at any point with:
 #st.session_state.name
 def func():
-    z=requests.post("https://api.hackathon.guardanthealth.com/dev/foundational-models/email",json={"patient_id": st.session_state.name})
+    z=requests.post("https://api.hackathon.guardanthealth.com/dev/foundational-models/email",json={"patient_id": option})
     return z.json()['draft_email']
 
 if submitted:
