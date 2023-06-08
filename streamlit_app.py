1cbfdf61204382f6489e68243bd5b1d207dbce12
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-08 23:39:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import requests
 st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
 st.session_state.name
+
+z=requests.get("https://api.fda.gov/drug/label.json?search=openfda.generic_name:'Erlotinib'").json()
+
+st.json(z, *, expanded=True)
