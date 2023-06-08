18c1a0f0c305a6e41ab4b0f76d2c9846985392ae
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-08 23:54:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import requests
-st.header("Patient Information")
-st.text_input("Name", key="name")
-st.text_input("Cancer type", key="cancer_type")
-st.text_input("Alteration", key="alteration")
-st.text_input("Approved therapy", key="therapy")
-st.text_input("Diagnostic type", key="diagnostic_type")
+
+with st.form("my_form"):
+    st.header("Patient Information")
+    st.text_input("Name", key="name")
+    st.text_input("Cancer type", key="cancer_type")
+    st.text_input("Alteration", key="alteration")
+    st.text_input("Approved therapy", key="therapy")
+    st.text_input("Diagnostic type", key="diagnostic_type")
+
+    submitted = st.form_submit_button("Submit")
 
 # You can access the value at any point with:
 #st.session_state.name
@@ -13,7 +17,7 @@ def func():
     z=requests.get("https://api.fda.gov/drug/label.json?search=openfda.generic_name:'Erlotinib'").json()
     return z
 
-st.form_submit_button(label="Submit", on_click=func())
-
-st.title("Email Response")
-st.json(z, expanded=True)
+if submitted:
+    z=func()
+    st.title("Email Response")
+    st.json(z, expanded=True)
