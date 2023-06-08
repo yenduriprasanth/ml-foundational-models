59faa2fb9cac04c9a100a2016a3dea44f512061f
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-08 23:45:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import requests
-st.text_input("Your name", key="name")
+st.header("Patient Information")
+st.text_input("Name", key="name")
+st.text_input("Cancer type", key="cancer_type")
+st.text_input("Alteration", key="alteration")
+st.text_input("Approved therapy", key="therapy")
+st.text_input("Diagnostic type", key="diagnostic_type")
 
 # You can access the value at any point with:
-st.session_state.name
+#st.session_state.name
 
 z=requests.get("https://api.fda.gov/drug/label.json?search=openfda.generic_name:'Erlotinib'").json()
 
+st.title("Email Response")
 st.json(z, expanded=True)
