e7ed381fe1226a2b318f17a29dc59c4a0424cd5d
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-23 01:04:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,21 @@ import requests
 with st.form("my_form"):
     st.header("Med Affairs Outreach Email Content Generator")
     st.text_input("Please enter Patient Id", key="id")
+    st.text_input("Please enter Number of Results", key="n_results")
     submitted = st.form_submit_button("Submit")
 
 # You can access the value at any point with:
 #st.session_state.name
 def func():
     z=requests.post("https://api.npdata.guardanthealth.com/dev/foundational-models/email",json={"patient_id": st.session_state.id})
-    return z.json()['draft_email']
-    #return z
+    #return z.json()['draft_email']
+    return z
 
 if submitted:
     z=func()
     st.title("Here's the email content ")
     #st.json(z, expanded=True)
-    z
+    for i in range(st.session_state.n_results):
+        z[i].json()['draft_email']
+        print("/n")
+    
