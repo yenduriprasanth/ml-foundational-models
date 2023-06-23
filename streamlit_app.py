5f324550dc99537ba40bc12dfd3152325f3d9a54
yenduriprasanth <31886613+yenduriprasanth@users.noreply.github.com>
2023-06-23 23:31:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import streamlit.components.v1 as components
 
 with st.form("my_form"):
     st.header("Med Affairs Outreach Email Content Generator")
@@ -13,6 +14,21 @@ def func():
     z=requests.post("https://api.npdata.guardanthealth.com/dev/foundational-models/email",json={"patient_id": st.session_state.id})
     #return z.json()['draft_email']
     return z.json()['draft_email']
+
+def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
+    htmlstr = f"""
+        <script>
+            var elements = window.parent.document.querySelectorAll('button');
+            for (var i = 0; i < elements.length; ++i) {{ 
+                if (elements[i].innerText == '{widget_label}') {{ 
+                    elements[i].style.color ='{font_color}';
+                    elements[i].style.background = '{background_color}'
+                }}
+            }}
+        </script>
+        """
+    components.html(f"{htmlstr}", height=0, width=0)
+
 
 
 
@@ -28,6 +44,7 @@ if submitted:
         st.button('Approve')
     with col3:
         st.button('Reject')
+    ChangeButtonColour('Edit', 'red', 'blue')
 
 #[st.button('Edit') ,    st.button('Approve')   , st.button('Reject')]
     #st.json(z, expanded=True)
